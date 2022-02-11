28f4cd59b1d193d3c41346bb92f97810131271da
Nayim hossen <nayimh2015@gmail.com>
2022-02-12 00:56:50 +0600

    number and dot

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ const operationEl = document.querySelectorAll('.operation');
 const equalEl = document.querySelector('.equal');
 const clearAllEl = document.querySelector('.all-clear');
 const clearLastEl = document.querySelector('.last-entity-clear');
+
+let dis1Num = '';
+let dis2Num = '';
+let result = null;
+let lastOperation = '';
+let haveDot = false;
+
+numbersEl.forEach(number => {
+    number.addEventListener('click', (e) => {
+        if (e.target.innerText === '.' && !haveDot) {
+            haveDot = true;
+        } else if (e.target.innerText === '.' && haveDot) {
+            return;
+        }
+        dis2Num += e.target.innerText;
+        display2El.innerText = dis2Num;
+    })
+})
